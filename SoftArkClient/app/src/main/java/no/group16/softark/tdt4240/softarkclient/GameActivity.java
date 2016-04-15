0db927c958ff6a4b5f3188ef247fd52fb36a2cec
Even Rognlien <even.rognlien@gmail.com>
2016-04-15 15:04:09 +0200

    Some cleanup and added some comments

--- SoftArkClient/app/src/main/java/no/group16/softark/tdt4240/softarkclient/GameActivity.java
+++ SoftArkClient/app/src/main/java/no/group16/softark/tdt4240/softarkclient/GameActivity.java
@@ -1,21 +1,15 @@
 package no.group16.softark.tdt4240.softarkclient;
 
 import android.graphics.Color;
-import android.graphics.ImageFormat;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.Icon;
-import android.support.annotation.Nullable;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,68 +19,93 @@ public class GameActivity extends AppCompatActivity {
 
     private TableRow keyRow1;
     private TableRow keyRow2;
-    private LinearLayout enteredLettersBar;
+    private TableRow enteredLettersRow;
 
     private TextView playerListTextView;
 
+    private final int TOT_AVAIL_CHAR = 13;
+    private final int MAX_CHAR_PER_ROW = 13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        // Hides system status/action bar -
         getSupportActionBar().hide();
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
         decorView.setSystemUiVisibility(uiOptions);
-
+        // --------------------------------
 
         keyRow1 = (TableRow)findViewById(R.id.keyboardRow1);
         keyRow2 = (TableRow)findViewById(R.id.keyboardRow2);
-        enteredLettersBar = (LinearLayout)findViewById(R.id.enteredLettersBar);
+        enteredLettersRow = (TableRow)findViewById(R.id.enteredLettersRow);
 
         playerListTextView = (TextView)findViewById(R.id.playersTextView);
 
-        updatePlayerList();
-        generateKeyboard(generateLetters("pannekake"));
-
+        updatePlayerList(/*TODO: pass JSON or something?*/);    // TODO: server should trigger this
+        setNewWord("pannekake");                                // TODO: server should trigger this
     }
 
-    private String generateLetters(String originalWord) {
+    /**
+     * MAYBE THIS SHOULD BE DONE ON THE SERVER INSTEAD, SO ALL USERS GET THE SAME?
+     * Take the original word, add some random letters and shuffle all
+     * @param originalWord
+     * @return Shuffled string containing the original word + random letters
+     */
+    /*private String generateLetters(String originalWord) {
         String letterSet = originalWord;
         Random r = new Random();
-        while(letterSet.length() < 13) {
+        while(letterSet.length() < TOT_AVAIL_CHAR) {
             char c = (char)(r.nextInt(26) + 'a');
             letterSet += c;
         }
         return shuffle(letterSet);
-    }
+    }*/
 
-    private void generateKeyboard(String word) {
-
-        word = word.toLowerCase();
+    /**
+     * Takes a string (should already be shuffled) and adds the containing letters to the keyboard
+     * @param string
+     */
+    private void generateKeyboard(String string) {
+        string = string.toLowerCase();
         int count = 0;
 
-        for(int i = 0; i < word.length(); i++) {
+        for(int i = 0; i < string.length(); i++) {
             ImageButton imgBtn = new ImageButton(this);
             imgBtn.setBackgroundColor(Color.TRANSPARENT);
-            char c = word.charAt(i);
+            char c = string.charAt(i);
             imgBtn.setImageResource(getResources().getIdentifier(c + "_grey", "drawable", getPackageName()));
             imgBtn.setMinimumHeight(0);
             imgBtn.setMinimumWidth(0);
             imgBtn.setPadding(2,4,2,2);
 
-            if(count < 13)
+            if(count < MAX_CHAR_PER_ROW)
                 keyRow1.addView(imgBtn);
             else
                 keyRow2.addView(imgBtn);
 
+            imgBtn.setTag(c);   // The letter is stored in the tag
+
             imgBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ((TableRow)v.getParent()).removeView(v);
+
                     ImageButton btn = (ImageButton)v;
-                    enteredLettersBar.addView(btn);
+
+                    if(btn.getParent() != enteredLettersRow) {
+                        ((TableRow)v.getParent()).removeView(v);
+                        enteredLettersRow.addView(btn);
+                    }
+                    else{
+                        ((TableRow)v.getParent()).removeView(v);
+
+                        if(keyRow1.getChildCount() < MAX_CHAR_PER_ROW)
+                            keyRow1.addView(btn);
+                        else
+                            keyRow2.addView(btn);
+                    }
                 }
             });
 
@@ -94,16 +113,24 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    private void updatePlayerList() {
+    /**
+     * Updates the list showing active players/teams and their scores
+     */
+    private void updatePlayerList(/*TODO: pass JSON or something?*/) {
         playerListTextView.setText(Html.fromHtml(
                 "Even (2300)<br>" +
                         "Viktor (2200)<br>" +
                         "Tom (2000)<br>" +
                         "Daniel (2000)<br>" +
-                        "Tien (1100)<br>"
+                        "Tien (1100)"
         ));
     }
 
+    /**
+     * Shuffles a string randomly
+     * @param input String to shuffle
+     * @return Shuffled string
+     */
     public String shuffle(String input){
         List<Character> characters = new ArrayList<Character>();
         for(char c:input.toCharArray()){
@@ -115,5 +142,27 @@ public class GameActivity extends AppCompatActivity {
             output.append(characters.remove(randPicker));
         }
         return output.toString();
+    }
+
+    /**
+     * MAYBE DO THIS ON THE SERVER INSTEAD?
+     * Checks if the user have guessed the correct word by looking at the entered characters
+     * @param correctWord The correct word
+     * @return true if answer matches correctWord
+     */
+    /*private boolean checkAnswer(String correctWord) {
+
+        String typedWord = "";
+
+        for (int i = 0; i < enteredLettersRow.getChildCount(); i++) {
+            ImageButton btn = (ImageButton)enteredLettersRow.getChildAt(i);
+            typedWord += btn.getTag();
+        }
+
+        return typedWord.equals(correctWord);
+    }*/
+
+    private void setNewWord(String letters) {
+        generateKeyboard(letters);
     }
 }
